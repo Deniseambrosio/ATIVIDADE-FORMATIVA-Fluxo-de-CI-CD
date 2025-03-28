4133810398148a25d612a72483c7a024fcac8868
Deniseambrosio <deniseambrosios@gmail.com>
2025-03-28 15:05:11 -0300

    rota itens adicionada

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "Hello, World!"
 
+@app.route("/items")
+def get_items():
+    return "item1, item2"
+
 if __name__ == '__main__':
     app.run(debug=True)
